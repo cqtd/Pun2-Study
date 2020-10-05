41aea420c139e0d95f01982c5f406e4d03f534c2
cqtd <cqtd@protonmail.ch>
2020-10-05 20:10:34 +0900

    Photon

---
+++ Assets/Scripts/Runtime/NetworkManager.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Photon.Pun;
+using Photon.Realtime;
+using UnityEngine;
+
+namespace CQ.Multiplay
+{
+	public class NetworkManager : MonoBehaviourPunCallbacks
+	{
+		void Start()
+		{
+			Screen.SetResolution(800, 600, false);
+			PhotonNetwork.ConnectUsingSettings();
+		}
+
+		public override void OnConnectedToMaster()
+		{
+			base.OnConnectedToMaster();
+
+			PhotonNetwork.JoinOrCreateRoom("Room", new RoomOptions() {MaxPlayers = 5}, null);
+			
+		}
+
+		public override void OnJoinedRoom()
+		{
+			base.OnJoinedRoom();
+
+			GameObject instance = PhotonNetwork.Instantiate("Player", Vector3.zero, Quaternion.identity);
+		}
+	}
+}
